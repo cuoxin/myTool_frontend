47ba0876b504fe694be4842cf46421473856b5da
Adrian <1984332305@qq.com>
2021-01-18 23:07:46 +0800

    :iphone: 增加状态栏组件

---
+++ src/components/StatusBar/StatusBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="top" :style="{ height: height }"></view>
+</template>
+<script>
+let statusBarHeight;
+uni.getSystemInfo({
+  success: res => {
+    statusBarHeight = res.statusBarHeight
+  }
+})
+
+export default {
+  data() {
+    return {
+      height: statusBarHeight + 'px'
+    };
+  }
+};
+</script>
+<style>
+.top {
+  width: 100%;
+}
+</style>
---
+++ src/pages/account/account.vue
@@ -0,0 +1,20 @@
+<template>
+  <view>
+    <status-bar></status-bar>
+    <view>{{ content }}</view>
+  </view>
+</template>
+
+<script>
+import StatusBar from "../../components/StatusBar/StatusBar.vue";
+
+export default {
+  components: { StatusBar },
+
+  data() {
+    return {
+      content: "Hello World!"
+    };
+  }
+};
+</script>
