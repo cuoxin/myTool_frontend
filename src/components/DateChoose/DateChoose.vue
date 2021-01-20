29c5e101b268ff3ff6e0d0a737cbc7cff5a991d8
Adrian <1984332305@qq.com>
2021-01-20 14:43:23 +0800

    :sparkles: 增加日期选择器

--- src/components/DateChoose/DateChoose.vue
+++ src/components/DateChoose/DateChoose.vue
@@ -1,6 +1,11 @@
 <template>
   <view>
-    <picker mode="date" fields="month" @change="chooseCallBack" :value="choose_date">
+    <picker
+      mode="date"
+      fields="month"
+      @change="chooseCallBack"
+      :value="choose_date"
+    >
       <view class="dateChose-showText">
         {{ choose_date }}
       </view>
@@ -13,17 +18,15 @@ let now_date = new Date();
 export default {
   data() {
     return {
-      choose_date: now_date.getFullYear() + '-' + (now_date.getMonth() + 1)
-    }
+      choose_date: now_date.getFullYear() + "-" + (now_date.getMonth() + 1)
+    };
   },
   methods: {
-
     // 时间选择后回调
-    chooseCallBack: function(event){
-      this.choose_date = event.detail.value
-      this.$emit('func', event.detail.value)
+    chooseCallBack: function(event) {
+      this.choose_date = event.detail.value;
+      this.$emit("func", event.detail.value);
     }
   }
-
-}
+};
 </script>
--- src/components/TimeNav/TimeNav.vue
+++ src/components/TimeNav/TimeNav.vue
@@ -2,7 +2,7 @@
   <view>
     <view class="timeNav-ico"></view>
     <view class="timeNav-timeChoose">
-      <date-choose @func='getChildrenDate' />
+      <date-choose @func="getChildrenDate" />
     </view>
   </view>
 </template>
@@ -13,8 +13,8 @@ export default {
 
   methods: {
     getChildrenDate: function(data) {
-      // TODO: 将数据保存，顺便更改选择器的样式，建议对日期选择增加更多的if
-      console.log(data)
+      // TODO: 将数据保存，顺便更改选择器的样式，建议对日期选择增加更多的参数
+      console.log(data);
     }
   }
 };
