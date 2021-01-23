da8b87b07f2772395b58e8ff7f57d1c05d9d9298
Adrian <1984332305@qq.com>
2021-01-23 23:47:07 +0800

    :iphone: 修改导航布局方式

--- src/pages/account/account.vue
+++ src/pages/account/account.vue
@@ -4,20 +4,20 @@
     <high-nav>
       <time-nav class="highNav-timeNav"></time-nav>
       <view class="account-downNav-box">
-        <view class="account-downNav-expense-box">
-          <view>
+        <view class="account-downNav-up">
+          <view class="account-downNav-expense-box">
             <view class="account-downNav-expense-title">支出</view>
             <view class="account-downNav-expense-content">{{ expense }}</view>
           </view>
-        </view>
-        <view class="account-downNav-income-box">
-          <view class="account-downNav-income-title">收入</view>
-          <view class="account-downNav-income-content">{{ income }}</view>
-        </view>
-        <view class="account-downNav-surplus-box">
-          <view>
+          <view class="account-downNav-surplus-box">
             <view class="account-downNav-surplus-title">结余</view>
             <view class="account-downNav-surplus-content">{{ surplus }}</view>
+          </view>
+        </view>
+        <view class="account-downNav-income-box">
+          <view class="account-downNav-income-box-">
+            <view class="account-downNav-income-title">收入</view>
+            <view class="account-downNav-income-content">{{ income }}</view>
           </view>
         </view>
       </view>
@@ -37,7 +37,7 @@ export default {
   data() {
     return {
       content: "Hello World!",
-      expense: 99999.99,
+      expense: 999999.99,
       income: 999999.99,
       surplus: 99999.99
     };
