5130c330bd75a5988d8aa34f29a2c9666f9025a4
Adrian <1984332305@qq.com>
2021-01-23 23:01:15 +0800

    :iphone: 主页导航样式完成

--- src/pages/account/account.vue
+++ src/pages/account/account.vue
@@ -3,6 +3,24 @@
     <status-bar :backgroundColor="'#6190E8'"></status-bar>
     <high-nav>
       <time-nav class="highNav-timeNav"></time-nav>
+      <view class="account-downNav-box">
+        <view class="account-downNav-expense-box">
+          <view>
+            <view class="account-downNav-expense-title">支出</view>
+            <view class="account-downNav-expense-content">{{ expense }}</view>
+          </view>
+        </view>
+        <view class="account-downNav-income-box">
+          <view class="account-downNav-income-title">收入</view>
+          <view class="account-downNav-income-content">{{ income }}</view>
+        </view>
+        <view class="account-downNav-surplus-box">
+          <view>
+            <view class="account-downNav-surplus-title">结余</view>
+            <view class="account-downNav-surplus-content">{{ surplus }}</view>
+          </view>
+        </view>
+      </view>
     </high-nav>
     <view>{{ content }}</view>
   </view>
@@ -18,7 +36,10 @@ export default {
 
   data() {
     return {
-      content: "Hello World!"
+      content: "Hello World!",
+      expense: 99999.99,
+      income: 999999.99,
+      surplus: 99999.99
     };
   }
 };
